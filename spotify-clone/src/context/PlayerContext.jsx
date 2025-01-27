dbc69fe445263ad4e1f86d8c5d89ede27a32c843
abhi-sharma368 <abhi.036807@gmail.com>
2025-01-27 21:30:49 +0530

    Update

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://spotify-clone-backend-bfer.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
